8fc48cc5eaa5145334f6770c259b5b029f75caa6
pcolar <dave@pcolar.com>
2019-11-06 13:27:01 -0500

    add error checking for token response

--- sierra_util.py
+++ sierra_util.py
@@ -5,11 +5,13 @@ import requests
 import json
 
 def get_token():
+    url = "https://catalog.chapelhillpubliclibrary.org/iii/sierra-api/v5/token"
 
-    # url = "https://sandbox.iii.com/iii/sierra-api/v5/token"
-    url = "https://catalog.chapelhillpubliclibrary.org/iii/sierra-api/v3/token"
+    # Get the API key from secrets file
     header = {"Authorization": "Basic " + str(secrets.sierra_api_2), "Content-Type": "application/x-www-form-urlencoded"}
     response = requests.post(url, headers=header)
+    if response.status_code != 200:
+        print(f'get Sierra Token failed with code {response.status_code} ')
+        exit(1)
     json_response = json.loads(response.text)
-    token = json_response["access_token"]
-    return token
+    return json_response["access_token"]
